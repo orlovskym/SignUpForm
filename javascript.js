dc34c02bf235b8352c5df870f3288b833570f975
Mark Orlovsky <orlovskym@gmail.com>
2022-11-08 20:09:32 -0800

    add color change to selected and error input elements

--- javascript.js
+++ javascript.js
@@ -10,15 +10,30 @@ const errorElement = document.getElementById('error')
 button.addEventListener('click', () => {
     if (first_name.value === '' || first_name == null) {
         errorElement.innerText='First name is required.'
+        first_name.className = " invalidElement"
     } else if (last_name.value === '' || last_name == null) {
         errorElement.innerText='Last name is required.'
+        last_name.className = " invalidElement"
     } else if (email.value === '' || email == null) {
         errorElement.innerText='Email is required.'
+        email.className = " invalidElement"
     } else if (phone.value === '' || phone == null) {
         errorElement.innerText='Phone number is required.'
+        phone.className = " invalidElement"
     } else if (password.value === '' || password == null) {
         errorElement.innerText='Password is required.'
+        password.className = " invalidElement"
     } else if (password.value !== password2.value) {
         errorElement.innerText='Passwords do not match.'
-    } else errorElement.innerText=''
+        password.className = " invalidElement"
+        password2.className = " invalidElement"
+    } else {
+        errorElement.innerText=''
+        first_name.classList.remove("invalidElement")
+        last_name.classList.remove("invalidElement")
+        email.classList.remove("invalidElement")
+        phone.classList.remove("invalidElement")
+        password.classList.remove("invalidElement")
+        password2.classList.remove("invalidElement")
+    }
     })
